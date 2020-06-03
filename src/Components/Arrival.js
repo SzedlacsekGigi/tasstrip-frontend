82165d42b1cf52b26ab36843625fa33ec0bc7fd7
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-06-03 23:30:14 +0200

    Switched out inside grids for cells

--- src/Components/Arrival.js
+++ src/Components/Arrival.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Grid} from "react-mdl";
+import {Cell, Grid} from "react-mdl";
 
 class Arrival extends Component {
 
@@ -7,45 +7,36 @@ class Arrival extends Component {
         return (
             <div className="arrival">
                 <Grid className="base-grid">
-                    <Grid className="inner-grid abcdef-box">
+                    <Cell className="inner-grid abcdef-box">
                         <p>{this.props.arrival.ssr}</p>
                         <p className="calls">{this.props.arrival.calls}</p>
                         <p className="type">{this.props.arrival.type}</p>
                         <p className="wtc">{this.props.arrival.wtc}</p>
                         <p className="mach-number">{this.props.arrival.machNumber}</p>
-                    </Grid>
-                    <Grid className="inner-grid arrival-g-box">
+                    </Cell>
+                    <Cell className="inner-grid arrival-g-box">
                         <p className="arrival-route-first">{this.props.arrival.route[0]}</p>
                         <p className="arrival-route-second">{this.props.arrival.route[1]}</p>
                         <p className="arrival-route-third">{this.props.arrival.route[2]}</p>
-                    </Grid>
-                    <Grid className="inner-grid arrival-hij-box">
+                    </Cell>
+                    <Cell className="inner-grid arrival-hij-box">
                         <p>{this.props.arrival.adep}</p>
                         <p className="arrival-ades">{this.props.arrival.ades}</p>
                         <p className="arrival-eta"></p>
-                    </Grid>
-                    <Grid className="inner-grid arrival-k-box">
+                    </Cell>
+                    <Cell className="inner-grid arrival-k-box">
                         <i className="material-icons">arrow_downward</i>
                         <p className="arrival-altitude">{this.props.arrival.levelsOrAltitude}</p>
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-l-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-m-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-n-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-o-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-p-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-q-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-r-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-small-box arrival-s-box">
-                    </Grid>
-                    <Grid className="inner-grid arrival-t-box">
-                    </Grid>
+                    </Cell>
+                    <Cell className="inner-grid arrival-small-box arrival-l-box"/>
+                    <Cell className="inner-grid arrival-small-box arrival-m-box"/>
+                    <Cell className="inner-grid arrival-small-box arrival-n-box"/>
+                    <Cell className="inner-grid arrival-small-box arrival-o-box"/>
+                    <Cell className="inner-grid arrival-small-box arrival-p-box"/>
+                    <Cell className="inner-grid arrival-small-box arrival-q-box"/>
+                    <Cell className="inner-grid arrival-small-box arrival-r-box"/>
+                    <Cell className="inner-grid arrival-small-box arrival-s-box"/>
+                    <Cell className="inner-grid arrival-t-box"/>
                 </Grid>
             </div>
         );
