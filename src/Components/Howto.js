abb6f4370f44041adb6d7c722e04bb09ec87515c
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-06-04 09:25:25 +0200

    How to page colors changed

--- src/Components/Howto.js
+++ src/Components/Howto.js
@@ -5,13 +5,18 @@ class Howto extends Component {
     render() {
         return (
             <div className="howto">
-                <h1>How to use this program</h1>
-                <h3>Step 1: Click on the Arrivals button to print the current arrivals, or click on the Departures button for printable departures</h3>
-                <h3>Step 2: In the Arrivals page, if you want to you can click on the designated place to type in the Arrival ETA.</h3>
-                <h3>Step 3: Right click anywhere on the page, then select "Print..."</h3>
-                <h5>Tip: In "More settings" you can choose the size of your paper, this program is designed for A/4</h5>
-                <h3>Click save.</h3>
-                <h3>Now you have your printable document!</h3>
+                <div className="howto-instructions">
+                    <h1>How to use this program</h1>
+                    <h3>Step 1: Click on the Arrivals button to print the current arrivals, or click on the Departures
+                        button for printable departures</h3>
+                    <h3>Step 2: In the Arrivals page, if you want to you can click on the designated place to type in
+                        the Arrival ETA.</h3>
+                    <h3>Step 3: Right click anywhere on the page, then select "Print..."</h3>
+                    <h5>Tip: In "More settings" you can choose the size of your paper, this program is designed for
+                        A/4</h5>
+                    <h3>Click save.</h3>
+                    <h3>Now you have your printable document!</h3>
+                </div>
             </div>
         );
     }
